af138d050312556c16c56777b130d4005b60a76e
Alain Mazy <am@osimis.io>
2018-01-09 10:36:50 +0100

    fix full-tls setup

--- docker/full-tls/tls/copy-tls-to-docker-volumes.sh
+++ docker/full-tls/tls/copy-tls-to-docker-volumes.sh
@@ -6,22 +6,22 @@ function writeFileToDockerVolume {
 }
 
 # copy server certs and private keys to volumes used by nginx to implement the TLS layer in front of Orthanc
-./_copy-tls-to-docker-volume.sh orthanc-a-server-crt.pem orthanc-a-server-key.pem orthancfulltls_orthanc-a-server-tls
-./_copy-tls-to-docker-volume.sh orthanc-b-server-crt.pem orthanc-b-server-key.pem orthancfulltls_orthanc-b-server-tls
+./_copy-tls-to-docker-volume.sh orthanc-a-server-crt.pem orthanc-a-server-key.pem fulltls_orthanc-a-server-tls
+./_copy-tls-to-docker-volume.sh orthanc-b-server-crt.pem orthanc-b-server-key.pem fulltls_orthanc-b-server-tls
 
 # nginx-b will perform some client certificate checks -> it needs the CA
 echo "Writing CA cert to nginx-b"
-writeFileToDockerVolume orthancfulltls_orthanc-b-server-tls ca-crt.pem < ca-crt.pem
+writeFileToDockerVolume fulltls_orthanc-b-server-tls ca-crt.pem < ca-crt.pem
 
-./_copy-tls-to-docker-volume.sh orthanc-a-client-crt.pem orthanc-a-client-key.pem orthancfulltls_orthanc-a-tls
+./_copy-tls-to-docker-volume.sh orthanc-a-client-crt.pem orthanc-a-client-key.pem fulltls_orthanc-a-tls
 echo "Writing CA cert to orthanc-a-tls"
-writeFileToDockerVolume orthancfulltls_orthanc-a-tls ca-crt.pem < ca-crt.pem
+writeFileToDockerVolume fulltls_orthanc-a-tls ca-crt.pem < ca-crt.pem
 
-./_copy-tls-to-docker-volume.sh orthanc-b-client-crt.pem orthanc-b-client-key.pem orthancfulltls_orthanc-b-forward-proxy-tls
+./_copy-tls-to-docker-volume.sh orthanc-b-client-crt.pem orthanc-b-client-key.pem fulltls_orthanc-b-forward-proxy-tls
 echo "Writing CA cert to orthanc-b-forward-proxy"
-writeFileToDockerVolume orthancfulltls_orthanc-b-forward-proxy-tls ca-crt.pem < ca-crt.pem
+writeFileToDockerVolume fulltls_orthanc-b-forward-proxy-tls ca-crt.pem < ca-crt.pem
 
-./_copy-tls-to-docker-volume.sh external-web-service-crt.pem external-web-service-key.pem orthancfulltls_external-web-service-tls
+./_copy-tls-to-docker-volume.sh external-web-service-crt.pem external-web-service-key.pem fulltls_external-web-service-tls
 echo "Writing CA cert to orthanc-b-forward-proxy"
-writeFileToDockerVolume orthancfulltls_external-web-service-tls ca-crt.pem < ca-crt.pem
+writeFileToDockerVolume fulltls_external-web-service-tls ca-crt.pem < ca-crt.pem
 
