bc0ec422460c51ff66fb28ecbc935d8bb3e49515
Alain Mazy <alain@mazy.be>
2024-09-16 13:01:30 +0200

    updated latency test

--- docker/postgresql-with-latency/README.md
+++ docker/postgresql-with-latency/README.md
@@ -4,11 +4,14 @@ This is a sample setup to demonstrate how to configure Orthanc with a PostgreSQL
 
 # Description
 
-|   | Beta 1 | 24.8.3  | 22.3.0    |
+|   | Beta 2 |  24.8.3  | 22.3.0    |
 |:---|---:|---:|---:|
-|/metadata (1st call) (Metadata) mode  |  4.069 |  6.459  | 11.052  |
-|/metadata (2nd call) (Metadata) mode  |  4.096 |  6.378   | 11.026  |
-|/metadata (1st call) (Full) mode  |  16.985|  22.362  | 9.640  |
-|/metadata (2nd call) (Full) mode  |  0.095 |  0.123   | 9.619  |
-|/studies?PatientID=*   |  0.026 |  0.712   |  0.076 |
-|/instances?PatientID=*   | 0.684  |  18.071   | 16.248  |
+|upload  | 9.379  |   14.260  | 15.443  |
+|/metadata (1st call) (MainDicomTags mode)  |  4.124 |    6.459  | 11.052  |
+|/metadata (2nd call) (MainDicomTags mode)  |  4.191 |    6.378   | 11.026  |
+|/metadata (1st call) (Full mode)  |  13.562 |    22.362  | 9.640  |
+|/metadata (2nd call) (Full mode)  |  0.090 |    0.123   | 9.619  |
+|/studies?PatientID=* (1st call)   |  0.030 |   0.756   |  0.076 |
+|/studies?PatientID=* (2nd call)  |  0.032 |   0.755   |  0.072 |
+|/instances?PatientID=* (1st call) |  0.733 |   18.071   | 16.248  |
+|/instances?PatientID=* (2nd call)  |  0.751 |   22.035   | 16.313  |
--- docker/postgresql-with-latency/test.sh
+++ docker/postgresql-with-latency/test.sh
@@ -5,38 +5,41 @@ docker compose up -d
 # might need to wait longer the first time !
 sleep 10
 
-curl http://localhost:8040/instances/ --data-binary @Study.zip > /dev/null
-curl http://localhost:8043/instances/ --data-binary @Study.zip > /dev/null
-curl http://localhost:8044/instances/ --data-binary @Study.zip > /dev/null
+# time curl http://localhost:8040/instances/ --data-binary @Study.zip > /dev/null
+# time curl http://localhost:8043/instances/ --data-binary @Study.zip > /dev/null
+# time curl http://localhost:8044/instances/ --data-binary @Study.zip > /dev/null
 
 
 study_id=1.2.276.0.7230010.3.1.2.380373504.1.1597664635.911712
 
+test() {
+    port=$1
+
+    echo "upload"
+    time curl http://localhost:$port/instances/ --data-binary @Study.zip > /dev/null
+
+    # two calls to analyze cache effect
+    echo "first call to /metadata"
+    time curl http://localhost:$port/dicom-web/studies/$study_id/metadata > /dev/null
+    echo "second call to /metadata"
+    time curl http://localhost:$port/dicom-web/studies/$study_id/metadata > /dev/null
+    # QIDO-RS
+    echo "first call to /studies?PatientID="
+    time curl "http://localhost:$port/dicom-web/studies?PatientID=*&includefield=NumberOfStudyRelatedInstances" > /dev/null
+    echo "second call to /studies?PatientID="
+    time curl "http://localhost:$port/dicom-web/studies?PatientID=*&includefield=NumberOfStudyRelatedInstances" > /dev/null
+    echo "first call to /instances?PatientID="
+    time curl "http://localhost:$port/dicom-web/instances?PatientID=*" > /dev/null
+    echo "second call to /instances?PatientID="
+    time curl "http://localhost:$port/dicom-web/instances?PatientID=*" > /dev/null
+}
+
 echo "######## BETA ##########"
-port=8040
-# two calls to analyze cache effect
-time curl http://localhost:$port/dicom-web/studies/$study_id/metadata > /dev/null
-time curl http://localhost:$port/dicom-web/studies/$study_id/metadata > /dev/null
-# QIDO-RS
-time curl "http://localhost:$port/dicom-web/studies?PatientID=*&includefield=NumberOfStudyRelatedInstances" > /dev/null
-time curl "http://localhost:$port/dicom-web/instances?PatientID=*" > /dev/null
+test 8040
 
 echo "######## STABLE ##########"
-port=8043
-# two calls to analyze cache effect
-time curl http://localhost:$port/dicom-web/studies/$study_id/metadata > /dev/null
-time curl http://localhost:$port/dicom-web/studies/$study_id/metadata > /dev/null
-# QIDO-RS
-time curl "http://localhost:$port/dicom-web/studies?PatientID=*&includefield=NumberOfStudyRelatedInstances" > /dev/null
-time curl "http://localhost:$port/dicom-web/instances?PatientID=*" > /dev/null
-
+test 8043
 
 echo "######## OLD ##########"
-port=8044
-# two calls to analyze cache effect
-time curl http://localhost:$port/dicom-web/studies/$study_id/metadata > /dev/null
-time curl http://localhost:$port/dicom-web/studies/$study_id/metadata > /dev/null
-# QIDO-RS
-time curl "http://localhost:$port/dicom-web/studies?PatientID=*&includefield=NumberOfStudyRelatedInstances" > /dev/null
-time curl "http://localhost:$port/dicom-web/instances?PatientID=*" > /dev/null
+test 8044
 
