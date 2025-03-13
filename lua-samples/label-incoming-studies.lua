1ff3f114b48b10b738550516f63ef7b7ca230c40
Alain Mazy <alain@mazy.be>
2025-03-13 12:29:08 +0100

    fix

--- lua-samples/label-incoming-studies.lua
+++ lua-samples/label-incoming-studies.lua
@@ -1,20 +1,22 @@
 function makeValidLabel(input)
     local invalidCharacters = "[^%w_-]"   -- everything that is not alphanumeric or _ or -
     return input:gsub(invalidCharacters, "-")
-  end
+end
   
-  function OnStoredInstance(instanceId, tags, metadata)
+function OnStoredInstance(instanceId, tags, metadata)
     print("OnStoredInstance ...")
     PrintRecursive(metadata)
   
     local parentStudy = ParseJson(RestApiGet("/instances/" .. instanceId .. "/study"))
-    if #parentStudy.Labels == 0 then
-      print("OnStoredInstance adding labels to parent study")
-      RestApiPut("/studies/" .. parentStudy.ID .. "/labels/called-" .. makeValidLabel(metadata.CalledAET), '')
-      RestApiPut("/studies/" .. parentStudy.ID .. "/labels/ip-" .. makeValidLabel(metadata.RemoteIP), '')
-      RestApiPut("/studies/" .. parentStudy.ID .. "/labels/remote-" .. makeValidLabel(metadata.RemoteAET), '')
+
+    if #parentStudy.Labels == 0 and metadata.RemoteAET ~= nil and metadata.RemoteAET ~= '' then
+      
+        print("OnStoredInstance adding labels to parent study")
+        -- RestApiPut("/studies/" .. parentStudy.ID .. "/labels/called-" .. makeValidLabel(metadata.CalledAET), '')
+        -- RestApiPut("/studies/" .. parentStudy.ID .. "/labels/ip-" .. makeValidLabel(metadata.RemoteIP), '')
+        RestApiPut("/studies/" .. parentStudy.ID .. "/labels/remote-" .. makeValidLabel(metadata.RemoteAET), '')
     end
   
     print("OnStoredInstance ... done")
   
-  end
+end
